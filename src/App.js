88d6d75948442e2dd5b42f3fff0f7858e4cdd323
DRSUS <jhinesgu99@gmail.com>
2020-10-09 22:12:47 -0400

    navigation for submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Link} from 'react-router-dom';
 import './App.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>TRIBAL<br/>NETWORK</h1>
+        <h2>The Network for the modern entertainer</h2>
+
+
+        <div style={{margin: '500px auto 400px'}} className="navigation">
+        <div>
+        <button style={{cursor: 'pointer'}} class='button'>LIVE</button>
+        </div>
+
+        <div>
+        <button style={{cursor: 'pointer'}} class='button'>Log In</button>
+        </div>
+
+        <div>
+        <button as={Link} to={'/'} style={{cursor: 'pointer'}} className="button">Submit</button>
+        </div>
+        </div>
+        
+        
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+
   );
+
 }
+
 
 export default App;
